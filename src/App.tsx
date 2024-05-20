1afd02792980333883da0af130e55513888c66e0
Thabzmy <salut98@hotmail.fr>
2024-05-20 20:26:12 +0200

    delete all

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
+
 import './App.css';
-import ManagingAdd from './components/Managing/ManagingAdd/ManagingAdd';
-import ManagingTable from './components/Managing/ManagingTable/ManagingTable';
 
 function App() {
+
   return (
     <div className="App">
-      <h1>Managing Table</h1>
-      <ManagingAdd />
-      <ManagingTable />
     </div>
   );
 }
